01332756c5b9bf72eaca5ead75b4fbb789f6e69d
Bria Williams <bria.williams128@gmail.com>
2021-03-24 18:26:18 -0400

    fixed email functionality

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-	
-	    
+
     /* ======= jQuery form validator ======= */ 
     /* Ref: http://jqueryvalidation.org/documentation/ */   
     $("#contact-form").validate({
@@ -21,4 +20,20 @@ $(document).ready(function() {
 	});
     
 
+});
+
+
+document.getElementById("send-now").addEventListener("click", function() {
+  var cname =  document.getElementById("cname").value;
+  var cemail = document.getElementById("cemail").value;
+  var reason = document.getElementById("services").value;
+  var cmessage = document.getElementById("cmessage").value;
+  const url = 'http://127.0.0.1:5000/contact';
+  var xhr = new XMLHttpRequest();
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const event = new Date();
+
+xhr.open("POST", url, true);
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.send(JSON.stringify({ name: cname, from: cemail, reason: reason, time: event.toLocaleDateString(undefined, options), text: cmessage}));
 });
