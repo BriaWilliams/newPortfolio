241c9dd86b530d4e83b1f1642f629bbeb546294e
Bria Williams <bria.williams128@gmail.com>
2021-03-25 17:44:59 -0400

    fixing flask messages on contact

--- assets/js/form.js
+++ assets/js/form.js
@@ -23,18 +23,40 @@ $(document).ready(function() {
 });
 
 
-document.getElementById("send-now").addEventListener("click", function() {
+document.getElementById("send-now").addEventListener("click", function(event) {
+  const SERVER_URL = 'https://email-controller.herokuapp.com/contact';
+  const POST_METHOD = 'POST';
+  const isAsync = true;
+  event.preventDefault();
   var cname =  document.getElementById("cname").value;
   var cemail = document.getElementById("cemail").value;
   var reason = document.getElementById("services").value;
   var cmessage = document.getElementById("cmessage").value;
-  const url = 'http://127.0.0.1:5000/contact';
   var xhr = new XMLHttpRequest();
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  const event = new Date();
+  const date = new Date();
 
-xhr.open("POST", url, true);
+xhr.open(POST_METHOD, SERVER_URL, isAsync);
 xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.preventDefault()
-xhr.send(JSON.stringify({ name: cname, from: cemail, reason: reason, time: event.toLocaleDateString(undefined, options), text: cmessage}));
+xhr.onreadystatechange = function() {//Call a function when the state changes.
+  var alert = document.getElementById("alert");
+    if(xhr.readyState == 4 && xhr.status == 200) {
+      // show alert if success
+      var message = JSON.parse(xhr.responseText).message;
+      document.getElementById("paragraph").textContent = message;
+      alert.style.display = "block";
+      // clean form
+      document.getElementById("cname").value='';
+      document.getElementById("cemail").value='';
+      document.getElementById("services").value='';
+      document.getElementById("cmessage").value='';
+    } else {
+      alert.classList.remove("alert-success")
+      alert.classList.add("alert-danger")
+      var alert =  document.getElementById("alert");
+      alert.style.display = "block";
+      document.getElementById("paragraph").textContent = 'Some error occurred. Please try again.';
+    }
+}
+xhr.send(JSON.stringify({ name: cname, from: cemail, reason: reason, time: date.toLocaleDateString(undefined, options), text: cmessage}));
 });
